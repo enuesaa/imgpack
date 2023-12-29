0f6c477a55c823e70a213c8d16fc9489aaf37284
nua <enuesaa@gmail.com>
2023-12-29 15:23:06 +0900

    feat: poc to compress png image

--- main.go
+++ main.go
@@ -1,7 +1,39 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"image"
+	"image/png"
+	"log"
+	"os"
+)
+
+func init() {
+	log.SetFlags(0)
+}
 
 func main() {
 	fmt.Printf("hello\n")
+
+	srcf, err := os.Open("out.png")
+	if err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+	defer srcf.Close()
+	srcimg, _, err := image.Decode(srcf)
+	if err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+
+	dstf, err := os.Create("out.png")
+	if err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
+
+	encoder := png.Encoder{
+		CompressionLevel: png.BestCompression,
+	}
+	if err := encoder.Encode(dstf, srcimg); err != nil {
+		log.Fatalf("Error: %s", err.Error())
+	}
 }
