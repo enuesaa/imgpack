9a3c58edb3297c910fc58315013413f9861cbc1b
nua <enuesaa@gmail.com>
2023-12-29 15:35:21 +0900

    feat: try to resize image

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/enuesaa/imgpack
 
 go 1.21.4
+
+require golang.org/x/image v0.14.0 // indirect
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"image"
+	"golang.org/x/image/draw"
 	"image/png"
 	"log"
 	"os"
@@ -12,6 +13,8 @@ func init() {
 	log.SetFlags(0)
 }
 
+// see https://zenn.dev/buyselltech/articles/202304091000
+// see https://text.baldanders.info/golang/resize-image/
 func main() {
 	fmt.Printf("hello\n")
 
@@ -30,10 +33,14 @@ func main() {
 		log.Fatalf("Error: %s", err.Error())
 	}
 
+	imgrect := srcimg.Bounds()
+	resized := image.NewRGBA(image.Rect(0, 0, imgrect.Dx()/2, imgrect.Dy()/2))
+	draw.CatmullRom.Scale(resized, resized.Bounds(), srcimg, imgrect, draw.Over, nil)
+
 	encoder := png.Encoder{
 		CompressionLevel: png.BestCompression,
 	}
-	if err := encoder.Encode(dstf, srcimg); err != nil {
+	if err := encoder.Encode(dstf, resized); err != nil {
 		log.Fatalf("Error: %s", err.Error())
 	}
 }
