e31a41c896c1ed6fa43c0e58271f6812563d13b6
nua <enuesaa@gmail.com>
2023-12-30 22:59:05 +0900

    chore: remove unused lines

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ import (
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
 	"github.com/cloudevents/sdk-go/v2/event"
-	// "github.com/enuesaa/imgpack/pkg/repository"
-	// "github.com/enuesaa/imgpack/pkg/usecase"
+	"github.com/enuesaa/imgpack/pkg/repository"
+	"github.com/enuesaa/imgpack/pkg/usecase"
 	"github.com/googleapis/google-cloudevents-go/cloud/storagedata"
 	"google.golang.org/protobuf/encoding/protojson"
 )
@@ -18,24 +18,17 @@ func init() {
 }
 
 func hanldeConvert(ctx context.Context, e event.Event) error {
-	log.Printf("Event ID: %s", e.ID())
-	log.Printf("Event Type: %s", e.Type())
-
 	var data storagedata.StorageObjectData
 	if err := protojson.Unmarshal(e.Data(), &data); err != nil {
 		return fmt.Errorf("Error: failed to unmarshal event data. %w", err)
 	}
-
-	log.Printf("Bucket: %s", data.GetBucket())
-	log.Printf("File: %s", data.GetName())
-	log.Printf("Metageneration: %d", data.GetMetageneration())
-	log.Printf("Created: %s", data.GetTimeCreated().AsTime())
-	log.Printf("Updated: %s", data.GetUpdated().AsTime())
+	filename := data.GetName()
+	log.Printf("File: %s", filename)
 	
-	// repos := repository.NewRepos()
-	// if err := usecase.Convert(repos, data.GetName()); err != nil {
-	// 	return fmt.Errorf("Error: %w", err)
-	// }
+	repos := repository.NewRepos()
+	if err := usecase.Convert(repos, filename); err != nil {
+		return fmt.Errorf("Error: %w", err)
+	}
 
 	return nil
 }
