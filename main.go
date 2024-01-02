6c9f925cd1ee2749b86d9fdafb9fa59b47ff2eff
nua <enuesaa@gmail.com>
2024-01-02 20:35:10 +0900

    feat: use http trigger because flow of `cloud storage -> functions -> cloud storage storage` may cause infinite loop and i want to fully avoid those situation

--- main.go
+++ main.go
@@ -1,34 +1,56 @@
 package hello
 
 import (
-	"context"
-	"fmt"
-	"log"
+	"encoding/json"
+	"io"
+	"net/http"
 
 	"github.com/GoogleCloudPlatform/functions-framework-go/functions"
-	"github.com/cloudevents/sdk-go/v2/event"
 	"github.com/enuesaa/imgpack/pkg/repository"
 	"github.com/enuesaa/imgpack/pkg/usecase"
-	"github.com/googleapis/google-cloudevents-go/cloud/storagedata"
-	"google.golang.org/protobuf/encoding/protojson"
 )
 
 func init() {
-	functions.CloudEvent("ImgpackConvert", hanldeConvert)
+	functions.HTTP("ImgpackConvert", hanldeConvert)
 }
 
-func hanldeConvert(ctx context.Context, e event.Event) error {
-	var data storagedata.StorageObjectData
-	if err := protojson.Unmarshal(e.Data(), &data); err != nil {
-		return fmt.Errorf("Error: failed to unmarshal event data. %w", err)
-	}
-	filename := data.GetName()
-	log.Printf("File: %s", filename)
-	
-	repos := repository.NewRepos()
-	if err := usecase.Convert(repos, filename); err != nil {
-		return fmt.Errorf("Error: %w", err)
+type ConvertFuncRequestBody struct {
+	Filename string `json:"filename"`
+}
+type ConvertFuncResponseBody struct {
+	Converted bool   `json:"converted"`
+	Filename  string `json:"filename"`
+}
+
+func hanldeConvert(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		http.Error(w, "Not Found", http.StatusNotFound)
+		return
 	}
 
-	return nil
+	reqbodybytes, err := io.ReadAll(r.Body)
+	if err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+	var reqbody ConvertFuncRequestBody
+	if err := json.Unmarshal(reqbodybytes, &reqbody); err != nil {
+		http.Error(w, "Error reading request body", http.StatusInternalServerError)
+		return
+	}
+
+	repos := repository.NewRepos()
+	if err := usecase.Convert(repos, reqbody.Filename); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	res := ConvertFuncResponseBody{
+		Filename:  reqbody.Filename,
+		Converted: true,
+	}
+	w.Header().Set("Content-Type", "application/json")
+	if err := json.NewEncoder(w).Encode(res); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
 }
