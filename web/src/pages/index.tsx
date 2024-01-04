7d5defc6988e57f49eedef51f44666822536d983
nua <enuesaa@gmail.com>
2024-01-04 14:55:38 +0900

    feat(web): firebase auth でのログイン状態を使いやすいよう保持していくための state を実装

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,8 +1,12 @@
 import { Header } from '@/components/common/Header'
 import { Main } from '@/components/common/Main'
 import { FileUpload } from '@/components/file/FileUpload'
+import { useUserAtomValue } from '@/firebase/auth'
 
 export default function TopPage() {
+  const user = useUserAtomValue()
+  console.log(user)
+
   return (
     <>
       <Header />
