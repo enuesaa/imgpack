f6aafabd757fcbb50168a84f4a4a2da2e8975526
nua <enuesaa@gmail.com>
2024-03-19 23:20:38 +0900

    feat: use svelte in this app for the interest

---
+++ web/src/App.svelte
@@ -0,0 +1,14 @@
+<script>
+	import { QueryClient, QueryClientProvider } from '@sveltestack/svelte-query'
+	import Todos from './components/Todos.svelte'
+
+	const queryClient = new QueryClient()
+</script>
+
+<main>
+	a
+
+	<!-- <QueryClientProvider client={queryClient}>
+		<Todos />
+	</QueryClientProvider>	 -->
+</main>
