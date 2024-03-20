db0c623ac436bfd103dd5b91658b27e641d9c88c
nua <enuesaa@gmail.com>
2024-03-21 01:59:32 +0900

    feat(web): use tailwindcss

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -9,6 +9,16 @@
 	}
 </script>
 
+<h1 class="text-3xl font-bold underline">
+	Hello world!
+</h1>
+  
+<style lang="postcss">
+	:global(html) {
+		background-color: theme(colors.gray.700);
+	}
+</style>
+
 <h2>{$count}</h2>
 <button on:click={updateCount}>+</button>
 
