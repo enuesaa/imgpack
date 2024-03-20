bedd62f7e1a0b3453d1e15be2b86c0602c390bc1
nua <enuesaa@gmail.com>
2024-03-20 17:57:09 +0900

    feat(web): use routify 3

--- web/src/App.svelte
+++ web/src/App.svelte
@@ -1,17 +1,16 @@
 <script>
-	import Router from 'svelte-spa-router'
-	import routes from './routes'
+    import { Router, createRouter } from '@roxi/routify'
+	// @ts-ignore
+    import routes from '../.routify/routes.default.js'
 
 	// import { QueryClient, QueryClientProvider } from '@sveltestack/svelte-query'
 	// import Todos from './components/Todos.svelte'
 	// const queryClient = new QueryClient()
 </script>
 
-<main>
-	<Router {routes} />
-	<!--
-	<QueryClientProvider client={queryClient}>
-		<Todos />
-	</QueryClientProvider>
-	-->
-</main>
+<Router {routes} />
+<!--
+<QueryClientProvider client={queryClient}>
+	<Todos />
+</QueryClientProvider>
+-->
--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,6 +1,20 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte'
+import routify from '@roxi/routify/vite-plugin'
 
 export default defineConfig({
-	plugins: [svelte()]
+	plugins: [
+		routify({
+			render: {
+				ssr: { enable: false },
+			},
+		}),
+		svelte({
+			compilerOptions: {
+				hydratable: !!process.env.ROUTIFY_SSR_ENABLE,
+			},
+			preprocess: vitePreprocess(),
+		}),
+	],
 })
