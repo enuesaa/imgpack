1fb9ad457ebc03ffb62b2381d7983279cf15a220
nua <enuesaa@gmail.com>
2024-03-20 22:25:43 +0900

    feat(web): try to use context

--- web/src/components/common/Header.svelte
+++ web/src/components/common/Header.svelte
@@ -1,5 +1,13 @@
 <script lang="ts">
 	export let title: string
+
+	type User = {
+		name: string
+	}
+
+	import { getContext } from 'svelte'
+	const user = getContext<User>('user')
 </script>
 
 <h1>{title}</h1>
+<p>Welcome {$user.name}</p>
