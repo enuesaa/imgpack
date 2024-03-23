9798cac961c2bc03495456c3d49a57f04c4d2f46
nua <enuesaa@gmail.com>
2024-03-23 16:11:52 +0900

    feat: switch serving frontend app

--- serve.go
+++ serve.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/enuesaa/imgpack/pkg/controller"
 	"github.com/enuesaa/imgpack/pkg/repository"
-	"github.com/enuesaa/imgpack/findui"
+	"github.com/enuesaa/imgpack/web"
 	"github.com/gofiber/fiber/v2"
 )
 
@@ -15,7 +15,7 @@ func Serve(repos repository.Repos, port int) error {
 	ctl := controller.New(repos)
 	app.Get("/api/files", ctl.ListFiles)
 	app.Post("/api/compress", ctl.Compress)
-	app.Get("/*", findui.Serve)
+	app.Get("/*", web.Serve)
 
 	addr := fmt.Sprintf("0.0.0.0:%d", port)
 	fmt.Printf("Serving web app on %s\n", addr)
