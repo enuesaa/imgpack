51af85589aa236a36f4d36c6295fecefce8fb775
nua <enuesaa@gmail.com>
2024-03-23 19:49:40 +0900

    chore: remove findui

--- findui/servedist.go
+++
@@ -1,5 +0,0 @@
-//go:build !dev
-
-package findui
-
-var Serve = ServeDist
--- findui/src/lib/path.ts
+++
@@ -1,6 +0,0 @@
-// TODO test this
-export const calcParentPath = (path: string): string => {
-  const splitted = path.split('/')
-  splitted.pop()
-  return splitted.join('/')
-}
