e35ef76a9be41e3555c4097fe4e5baee5dc4bb91
nua <enuesaa@gmail.com>
2024-03-23 18:08:40 +0900

    feat(web): add go to parent dir link

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,8 +1,10 @@
 <script lang="ts">
 	import { page } from '$app/stores'
 	import Files from './Files.svelte'
+	import GoToParentDirLink from './GoToParentDirLink.svelte'
 
-	const path = $page.url.searchParams.get('path') ?? '/'
+	const path = $page.url.searchParams.get('path') ?? './'
 </script>
 
+<GoToParentDirLink path={path} />
 <Files path={path} />
---
+++ web/src/routes/GoToParentDirLink.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+	import { calcParentPath } from '$lib/path'
+	import FaArrowUp from 'svelte-icons/fa/FaArrowUp.svelte'
+
+	export let path: string
+
+	const parentPath = calcParentPath(path)
+</script>
+
+<div class="my-5 relative h-5">
+	<a href={`/?path=` + parentPath} class="absolute left-10 h-5">
+		<FaArrowUp />
+	</a>
+</div>
