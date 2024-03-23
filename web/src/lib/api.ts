54d273664a6556ce731ccea7240978a1d7532830
nua <enuesaa@gmail.com>
2024-03-23 14:37:44 +0900

    feat(web): copied query defs from current react app

---
+++ web/src/lib/api.ts
@@ -0,0 +1,41 @@
+import { createQuery, createMutation } from '@tanstack/svelte-query'
+
+const apiBaseUrl = 'http://localhost:3000/api/'
+
+type ListFilesSchema = {
+  path: string
+  items: ListFilesSchemaItem[]
+}
+type ListFilesSchemaItem = {
+  name: string
+  isCompressable: boolean
+  isDir: boolean
+}
+export const useListFiles = (path: string) => createQuery({
+  queryKey: [`listFiles-${path}`],
+  queryFn: async (): Promise<ListFilesSchema> => {
+    const res = await fetch(`${apiBaseUrl}files?path=${path}`)
+    const body = await res.json()
+    return body
+  },
+})
+
+
+type CompressSchema = {
+  success: boolean
+}
+export const useCompress = () => createMutation({
+  mutationFn: async (filename: string): Promise<CompressSchema> => {
+    const res = await fetch(`${apiBaseUrl}/compress`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        filename,
+      }),
+    })
+    const body = await res.json()
+    return body
+  },
+})
