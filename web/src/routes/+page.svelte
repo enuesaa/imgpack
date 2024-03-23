7c1b84b0f8fd746f9333545bfbe5b4a84045d369
nua <enuesaa@gmail.com>
2024-03-23 17:04:19 +0900

    feat(web): get query string `path`

--- web/src/routes/+page.svelte
+++ web/src/routes/+page.svelte
@@ -1,7 +1,12 @@
 <script lang="ts">
 	import Header from './Header.svelte'
 	import Tree from './Tree.svelte'
+	import { page } from '$app/stores'
+
+	const path = $page.url.searchParams.get('path') ?? '/'
 </script>
 
+<div>{$page.url}</div>
+<div>{path}</div>
 <Header />
 <Tree />
