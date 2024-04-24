28beb4d43f253018f87b9393f823073920208e0a
nua <enuesaa@gmail.com>
2024-04-25 03:04:29 +0900

    chore: rename web package to ui package

--- serve.go
+++ serve.go
@@ -5,7 +5,7 @@ import (
 
 	"github.com/enuesaa/imgpack/pkg/controller"
 	"github.com/enuesaa/imgpack/pkg/repository"
-	"github.com/enuesaa/imgpack/web"
+	"github.com/enuesaa/imgpack/ui"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
 )
