e140fbcd333a34917b179f71354340236e181396
nua <enuesaa@gmail.com>
2024-04-25 02:39:09 +0900

    feat(web): add dnd area

--- web/src/routes/Upload.svelte
+++ web/src/routes/Upload.svelte
@@ -10,12 +10,31 @@
 			return
 		}
 		const file = files[0]
-		const filename = file.name
 		const formdata = new FormData()
 		formdata.append('file', file)
 		const compressedUrl = await $upload.mutateAsync(formdata)
-		addImage({ compressedUrl, filename })
+		addImage({ compressedUrl, filename: file.name })
+	}
+
+	async function handleDrop(e: DragEvent) {
+		const files = e?.dataTransfer?.files ?? null
+		if (files === null) {
+			return
+		}
+		const file = files[0]
+		const formdata = new FormData()
+		formdata.append('file', file)
+		const compressedUrl = await $upload.mutateAsync(formdata)
+		addImage({ compressedUrl, filename: file.name })
 	}
 </script>
 
-<input multiple type="file" on:change|preventDefault={handleUpload} />
+<section
+	class="w-full h-10 bg-white"
+	on:drop|preventDefault={handleDrop}
+	on:dragover|preventDefault
+	on:dragleave|preventDefault
+>
+	drop here
+	<input multiple type="file" on:change|preventDefault|stopPropagation={handleUpload} />
+</section>
