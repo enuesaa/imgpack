0730a0555773f552fcdcd31ea1652e915153d2f6
nua <enuesaa@gmail.com>
2024-04-25 03:02:50 +0900

    feat(cli): make web app. remove cli options.

--- main.go
+++ main.go
@@ -16,22 +16,15 @@ func main() {
 
 	app := &cobra.Command{
 		Use:   "imgpack",
-		Short:  "Web app to compress images. To serve, please pass `--serve` flag.",
-		Version: "0.0.4",
+		Short:  "Web app to compress images.",
+		Version: "0.0.5",
 		Run: func(cmd *cobra.Command, args []string) {
-			serve, _ := cmd.Flags().GetBool("serve")
 			port, _ := cmd.Flags().GetInt("port")
-
-			if serve {
-				if err := Serve(repos, port); err != nil {
-					log.Fatalf("Error: %s", err.Error())
-				}
-			} else {
-				cmd.Help()
+			if err := Serve(repos, port); err != nil {
+				log.Fatalf("Error: %s", err.Error())
 			}
 		},
 	}
-	app.Flags().Bool("serve", false, "serve web app")
 	app.Flags().Int("port", 3000, "port")
 
 	// disable default
