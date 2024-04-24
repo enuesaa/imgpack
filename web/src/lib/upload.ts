0fade60a3c755fc757bb70240aaadc8a3555459e
nua <enuesaa@gmail.com>
2024-04-25 02:56:30 +0900

    feat(upload): styling

--- web/src/lib/upload.ts
+++ web/src/lib/upload.ts
@@ -1,14 +1,23 @@
 import { createMutation } from '@tanstack/svelte-query'
+import { addImage } from './images'
 
 const apiBaseUrl = 'http://localhost:3000/api/'
 
 export const useUpload = () => createMutation({
-  mutationFn: async (formdata: FormData): Promise<string> => {
+  mutationFn: async (files: FileList) => {
+    const file = files[0]
+    const filename = file.name
+
+		const formdata = new FormData()
+		formdata.append('file', file)
+
     const res = await fetch(`${apiBaseUrl}upload`, {
       method: 'POST',
       body: formdata
-    });
+    })
     const compressed = await res.blob()
-    return URL.createObjectURL(compressed)
+
+    const compressedUrl = URL.createObjectURL(compressed)
+    addImage({ compressedUrl, filename })
   },
 })
--- web/src/routes/Upload.svelte
+++ web/src/routes/Upload.svelte
@@ -1,40 +1,38 @@
 <script lang="ts">
-	import { addImage } from '$lib/images'
 	import { useUpload } from '$lib/upload'
 
 	const upload = useUpload()
+	let inputfile: HTMLInputElement
 
 	async function handleUpload(e: Event & { currentTarget: HTMLInputElement }) {
 		const files = e.currentTarget.files
 		if (files === null) {
 			return
 		}
-		const file = files[0]
-		const formdata = new FormData()
-		formdata.append('file', file)
-		const compressedUrl = await $upload.mutateAsync(formdata)
-		addImage({ compressedUrl, filename: file.name })
+		await $upload.mutateAsync(files)
 	}
-
 	async function handleDrop(e: DragEvent) {
 		const files = e?.dataTransfer?.files ?? null
 		if (files === null) {
 			return
 		}
-		const file = files[0]
-		const formdata = new FormData()
-		formdata.append('file', file)
-		const compressedUrl = await $upload.mutateAsync(formdata)
-		addImage({ compressedUrl, filename: file.name })
+		await $upload.mutateAsync(files)
 	}
 </script>
 
 <section
-	class="w-full h-10 bg-white"
+	class="w-full rounded-md text-center p-20"
+	style="border: solid 1px #fafafa;"
 	on:drop|preventDefault={handleDrop}
 	on:dragover|preventDefault
 	on:dragleave|preventDefault
+	on:click={() => inputfile.click()}
 >
 	drop here
-	<input multiple type="file" on:change|preventDefault|stopPropagation={handleUpload} />
+	<input
+		bind:this={inputfile}
+		type="file" multiple
+		class="opacity-0 block"
+		on:change|preventDefault|stopPropagation={handleUpload}
+	/>
 </section>
