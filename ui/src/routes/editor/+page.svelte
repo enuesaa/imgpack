658ddc84f9c621e136d359b0f6609912801ca2a3
nua <enuesaa@gmail.com>
2025-06-01 15:56:46 +0900

    feat: use svelte 5

--- ui/src/routes/editor/+page.svelte
+++ ui/src/routes/editor/+page.svelte
@@ -1,50 +1,51 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
-
-	let canvas: HTMLCanvasElement
-	let ctx: CanvasRenderingContext2D|null
-	let outputCanvas: HTMLCanvasElement
-	let outputCtx: CanvasRenderingContext2D|null
+	let canvas = $state<HTMLCanvasElement>()
+	let ctx = $state<CanvasRenderingContext2D>()
+	let ocanvas = $state<HTMLCanvasElement>()
+	let octx = $state<CanvasRenderingContext2D>()
 
 	let img = new Image()
 	img.src = 'example.png'
-
-	let isDragging = false
-	let startX = 0
-	let startY = 0
-	let currentX = 0
-	let currentY = 0
-
-	onMount(() => {
-		img.onload = () => {
-			ctx = canvas.getContext('2d')
-			outputCtx = outputCanvas.getContext('2d')
-			canvas.width = img.width
-			canvas.height = img.height
-
-			if (ctx === null) {
-				return
-			}
-			ctx.drawImage(img, 0, 0)
+	img.onload = () => {
+		if (canvas === undefined || ocanvas === undefined) {
+			return
 		}
-	})
+		ctx = canvas.getContext('2d') ?? undefined
+		octx = ocanvas.getContext('2d') ?? undefined
 
-	function startSelection(event: MouseEvent) {
+		canvas.width = img.width
+		canvas.height = img.height
+		if (ctx === undefined) {
+			return
+		}
+		ctx.drawImage(img, 0, 0)
+	}
+
+	let isDragging = $state(false)
+	let startX = $state(0)
+	let startY = $state(0)
+	let currentX = $state(0)
+	let currentY = $state(0)
+
+	function handleMouseDown(event: MouseEvent) {
+		if (canvas === undefined || ocanvas === undefined) {
+			return
+		}
 		const rect = canvas.getBoundingClientRect()
 		startX = event.clientX - rect.left
 		startY = event.clientY - rect.top
 		isDragging = true
 	}
 
-	function updateSelection(event: MouseEvent) {
-		if (!isDragging) {
+	function handleMouseMove(event: MouseEvent) {
+		if (canvas === undefined || ocanvas === undefined || !isDragging) {
 			return
 		}
 		const rect = canvas.getBoundingClientRect()
 		currentX = event.clientX - rect.left
 		currentY = event.clientY - rect.top
 
-		if (ctx === null) {
+		if (ctx === undefined) {
 			return
 		}
 		ctx.clearRect(0, 0, canvas.width, canvas.height)
@@ -54,7 +55,10 @@
 		ctx.strokeRect(startX, startY, currentX - startX, currentY - startY)
 	}
 
-	function finishSelection() {
+	function handleMouseUp() {
+		if (canvas === undefined || ocanvas === undefined) {
+			return
+		}
 		isDragging = false
 
 		const x = Math.min(startX, currentX)
@@ -62,22 +66,25 @@
 		const width = Math.abs(currentX - startX)
 		const height = Math.abs(currentY - startY)
 
-		outputCanvas.width = width
-		outputCanvas.height = height
+		ocanvas.width = width
+		ocanvas.height = height
 
-		if (outputCtx === null) {
+		if (octx === undefined) {
 			return
 		}
-		outputCtx.drawImage(img, x, y, width, height, 0, 0, width, height)
+		octx.drawImage(img, x, y, width, height, 0, 0, width, height)
 	}
 </script>
 
-<canvas bind:this={canvas} on:mousedown={startSelection} on:mousemove={updateSelection} on:mouseup={finishSelection} />
-<canvas bind:this={outputCanvas} />
-
-<style>
-	canvas {
-		border: 1px solid #ccc;
-		margin: 10px;
-	}
-</style>
+<svelte:options runes={true} />
+<canvas
+	bind:this={canvas}
+	onmousedown={handleMouseDown}
+	onmousemove={handleMouseMove}
+	onmouseup={handleMouseUp}
+	class="border-white border my-1"
+></canvas>
+<canvas
+	bind:this={ocanvas}
+	class="border-white border my-1"
+></canvas>
