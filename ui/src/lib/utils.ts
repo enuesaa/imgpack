4dd1a43c749370c35fefc781eb12d960533abbab
nua <enuesaa@gmail.com>
2025-08-17 17:05:13 +0900

    chore: remove old codes. and move rust code to toplevel

--- ui/src/lib/utils.ts
+++
@@ -1,9 +0,0 @@
-type EventHandler = (e: any) => void
-
-// see https://svelte.dev/docs/svelte/v5-migration-guide
-export function preventdefault(fn: EventHandler): EventHandler {
-	return function (e: Event) {
-		e.preventDefault()
-		fn(e)
-	}
-}
--- ui/src/routes/+page.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-	import Compressed from './Compressed.svelte'
-	import Upload from './Upload.svelte'
-</script>
-
-<Upload />
-<Compressed />
