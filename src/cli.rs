72333e2a34ecf0eb76cb6936e8e5b08aec1ad6b4
nua <enuesaa@gmail.com>
2025-08-19 17:32:04 +0900

    feat(cli): update version name

--- src/cli.rs
+++ src/cli.rs
@@ -2,9 +2,9 @@ use std::path::PathBuf;
 
 pub use clap::Parser as CLIParser;
 
-/// A CLI tool to compress png images.
+/// A CLI tool to compress png/jpg images.
 #[derive(CLIParser)]
-#[command(bin_name = "imgpack", version = "v0.0.1")]
+#[command(bin_name = "imgpack", version = "v0.1.0")]
 pub struct CLI {
     /// directory to compress
     #[arg(value_name = "PATH", default_value = ".")]
