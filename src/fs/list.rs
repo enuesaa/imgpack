b6e16c0713e38e0af155aa6acc12cd3d24d2181c
nua <enuesaa@gmail.com>
2025-08-24 17:13:27 +0900

    feat: filter already compressed file

--- src/fs/list.rs
+++ src/fs/list.rs
@@ -1,9 +1,9 @@
-use std::fs;
+use std::{collections::HashSet, fs};
 
 use anyhow::Result;
 use std::path::PathBuf;
 
-use crate::fs::compressable::Compressable;
+use crate::fs::{compressable::Compressable, original::calc_originalpath};
 
 pub fn list_compressables(path: &PathBuf) -> Result<Vec<Compressable>> {
     let files = list_files_in_dir(path)?;
@@ -30,7 +30,8 @@ fn list_files_in_dir(path: &PathBuf) -> Result<Vec<PathBuf>> {
 }
 
 fn filter_compress_target(files: Vec<PathBuf>) -> Vec<PathBuf> {
-    files
+    let images: Vec<PathBuf> = files
+        .clone()
         .into_iter()
         .filter(|path| {
             if let Some(name) = path.file_name().and_then(|n| n.to_str()) {
@@ -39,12 +40,57 @@ fn filter_compress_target(files: Vec<PathBuf>) -> Vec<PathBuf> {
                 false
             }
         })
+        .collect();
+
+    let originals: HashSet<String> = images
+        .iter()
+        .filter_map(|path| {
+            path.file_name()
+                .and_then(|n| n.to_str())
+                .filter(|name| name.contains(".original."))
+                .map(|s| s.to_string())
+        })
+        .collect();
+
+    images
+        .into_iter()
         .filter(|path| {
             if let Some(name) = path.file_name().and_then(|n| n.to_str()) {
-                !name.contains(".original.")
+                !originals.contains(&name.to_string())
+            } else {
+                false
+            }
+        })
+        .filter(|path| {
+            if let Ok(original) = calc_originalpath(path) {
+                !originals.contains(&original.file_name().unwrap().to_str().unwrap().to_string())
             } else {
                 false
             }
         })
         .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_filter_compress_target() {
+        let files = vec![
+            PathBuf::from("a.png"), // 圧縮済みなのでスキップ
+            PathBuf::from("a.original.png"), // オリジナルファイルなのでスキップ
+            PathBuf::from("b.jpg"),
+            PathBuf::from("c.png"),
+            PathBuf::from("d.original.png"), // オリジナルファイルなのでスキップ
+        ];
+
+        let res = filter_compress_target(files);
+
+        let names: Vec<String> = res
+            .iter()
+            .map(|p| p.file_name().unwrap().to_str().unwrap().to_string())
+            .collect();
+        assert_eq!(names, vec!["b.jpg", "c.png"]);
+    }
 }
