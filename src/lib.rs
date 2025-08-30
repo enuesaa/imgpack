89f1a64d226df44f161aff9deceb87c8f6565b0f
nua <enuesaa@gmail.com>
2025-08-30 15:16:02 +0900

    fix: 所有権にミスがあった

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use fs::{Ext, list_compressables, Compressable};
 use pack::{pack_jpg, pack_png, rename_original};
 use std::{io::Write, path::PathBuf};
 
-pub fn compress_dir<W: Write>(path: PathBuf, mut logger: W) -> Result<()> {
+pub fn compress_dir<W: Write>(path: PathBuf, logger: &mut W) -> Result<()> {
     let files = list_compressables(&path)?;
 
     for file in files.iter() {
@@ -23,7 +23,7 @@ pub fn compress_dir<W: Write>(path: PathBuf, mut logger: W) -> Result<()> {
     Ok(())
 }
 
-pub fn compare_filesize<W: Write>(file: &Compressable, mut logger: W) -> Result<()> {
+pub fn compare_filesize<W: Write>(file: &Compressable, logger: &mut W) -> Result<()> {
     let out = file.get_out_filesize()?;
     let original = file.get_original_filesize()?;
     writeln!(logger, "original: {}, out: {}", original, out)?;
