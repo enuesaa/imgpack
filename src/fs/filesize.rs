f2f543a4de16ae26560c3fa5dbdbd635d9e04bb7
nua <enuesaa@gmail.com>
2025-09-07 17:48:47 +0900

    chore: run test to check testdata sample file does not change

--- src/fs/filesize.rs
+++ src/fs/filesize.rs
@@ -7,3 +7,19 @@ pub fn get_filesize(path: &PathBuf) -> Result<f64> {
     let kb = (bytes as f64 / 1000.0).round();
     Ok(kb)
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_get_filesize_testdata() {
+        let path = PathBuf::from("testdata/lakemountain.png");
+        let filesize = get_filesize(&path);
+        assert_eq!(filesize.unwrap(), 3157.0);
+        
+        let path = PathBuf::from("testdata/lakemountain.jpg");
+        let filesize = get_filesize(&path);
+        assert_eq!(filesize.unwrap(), 632.0);
+    }
+}
